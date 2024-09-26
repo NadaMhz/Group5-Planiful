e9e847babcab3f0d4a003cf0d8bdf268a97a26a4
Afnan Alonazi <146772610+afnanAlonazi@users.noreply.github.com>
2024-09-26 07:31:38 +0300

    Delete lib directory

--- lib/index.dart
+++
@@ -1,8 +0,0 @@
-// Export pages
-export '/home_page/home_page_widget.dart' show HomePageWidget;
-export '/search_for_plant/search_for_plant_widget.dart'
-    show SearchForPlantWidget;
-export '/maket/maket_widget.dart' show MaketWidget;
-export '/profile/profile_widget.dart' show ProfileWidget;
-export '/care/care_widget.dart' show CareWidget;
-export '/my_plant/my_plant_widget.dart' show MyPlantWidget;
